f832e26ddcf6cebea04670a34f100729923ec5c1
volards <35000534+volards@users.noreply.github.com>
2023-05-20 10:41:02 +0300

    Update one.py

--- one.py
+++ one.py
@@ -1,7 +1,7 @@
 def quick_job():
-    with open("wallets_and_ammounts.txt", "r") as f:
-        wallets_and_ammounts_list = [l.strip() for l in f]
-    wallets = [l.split(" ")[0] for l in wallets_and_ammounts_list]
+    with open("wallets_and_amounts.txt", "r") as f:
+        wallets_and_amounts_list = [l.strip() for l in f]
+    wallets = [l.split(" ")[0] for l in wallets_and_amounts_list]
     wallets_sorted_by_frequency = sorted(wallets, key=lambda l: wallets.count(l), reverse=True)
     checked_wallets = []
     final_dict = {}
@@ -9,20 +9,18 @@ def quick_job():
     for wallet in wallets_sorted_by_frequency:
         if wallet not in checked_wallets:
             if wallets.count(wallet) > 8:
-                this_wallet_ammounts_list = [l for l in wallets_and_ammounts_list if wallet == l.split(" ")[0]]
-                first_part, second_part = this_wallet_ammounts_list[:8], this_wallet_ammounts_list[8:]
-                # print(first_part, second_part)
+                this_wallet_amounts_list = [l for l in wallets_and_amounts_list if wallet == l.split(" ")[0]]
+                first_part, second_part = this_wallet_amounts_list[:8], this_wallet_amounts_list[8:]
                 for k in range(0, 8):
                     final_dict[k*43 + len(checked_wallets)] = first_part[k]
                 add_to_the_end.extend(second_part)
             else:
-                this_wallet_ammounts_list = [l for l in wallets_and_ammounts_list if wallet == l.split(" ")[0]]
-                # print(this_wallet_ammounts_list)
+                this_wallet_amounts_list = [l for l in wallets_and_amounts_list if wallet == l.split(" ")[0]]
                 for k in range(0, wallets.count(wallet)):
                     place_to_put = 0
                     while (place_to_put + (k*43 + len(checked_wallets))) in final_dict:
                         place_to_put +=1
-                    final_dict[place_to_put + k*43 + len(checked_wallets)] = this_wallet_ammounts_list[k]
+                    final_dict[place_to_put + k*43 + len(checked_wallets)] = this_wallet_amounts_list[k]
             checked_wallets.append(wallet)
     final_list = [v for k, v in sorted(final_dict.items())]
     final_list.extend(add_to_the_end)
